e4fc626be4bebc1ae39ae4e2526fdf592e463c2b
Kuramenai <marcelynsteeve@gmail.com>
2022-10-12 17:06:25 +0800

    Can now change the size of the grid by using a slider
    The createGrid() function is now createGridCells(), we use it
    to create the grid cells of an already existing grid with default dims
    when the user opens the website. Plus we have added a new function
    createNewGrid() to create and return a new whole grid with the size specified by the user.
    This function is used by the updateGrid() function which replaces
    the existing grid by the new one created by createNewGrid().
    This function itself is called every time the user modifies the value of the slider.

--- javascript/script.js
+++ javascript/script.js
@@ -1,12 +1,22 @@
-const grid = document.querySelector('.grid_container');
+const grid_container = document.querySelector('.grid_container');
+let grid = document.querySelector('.grid');
 
 let startDrawing = false;
 let cell_color = 'orange'
 
 
+
+let change_grid_size_slider = document.querySelector('.change-grid-size-slider');
+change_grid_size_slider.oninput = getGridSize
+
+let grid_size = 480;
+let grid_rows_columns = 16;
+
+
 //When the user clicks on a cell then start hovering on the whole grid
 //while maintaining the mouse left click button pressed down
 //All the "hovered" cells background color will change to the selected one
+
 //isStartDrawing is used to identify the start of a drawing
 function isStartDrawing(event){
 
@@ -39,18 +49,77 @@ function draw(event){
    
 }
 
-
-//Generate the grid cells and add the necessary event listeners
-for(let i = 0; i < 256; i++){
+//Generate the grid cells and add the necessary event listeners at  website launch 
+function createGridCells()
+{
     
-    grid_cell = document.createElement('div');
+    
+    for(let i = 0; i < grid_rows_columns*grid_rows_columns ; i++){
+    
+        grid_cell = document.createElement('div');
+    
+        grid_cell.classList.add('grid_square');
+    
+        grid_cell.addEventListener('mousedown',isStartDrawing)
+        grid_cell.addEventListener('mouseenter',draw);
+        grid_cell.addEventListener('mouseup',isEndDrawing);
 
-    grid_cell.classList.add('grid_square');
+    
+        grid.appendChild(grid_cell);
+    }
+    
 
-    grid_cell.addEventListener('mousedown',isStartDrawing)
-    grid_cell.addEventListener('mouseenter',draw);
-    grid_cell.addEventListener('mouseup',isEndDrawing);
-
-    grid.appendChild(grid_cell);
 }
+
+//Create a new grid with size chosen by user 
+function createNewGrid(){
+
+    let cell_pixel_size = grid_size/grid_rows_columns;
+    const grid = document.createElement('div');
+
+    grid.setAttribute('style',  `display:grid;
+                                 grid-template-columns:repeat(${grid_rows_columns},${cell_pixel_size}px);\
+                                 grid-template-rows:repeat(${grid_rows_columns},${cell_pixel_size}px);`);
+
+    for(let i = 0; i < grid_rows_columns*grid_rows_columns ; i++){
+    
+        grid_cell = document.createElement('div');
+    
+        grid_cell.classList.add('grid_square');
+    
+        grid_cell.addEventListener('mousedown',isStartDrawing)
+        grid_cell.addEventListener('mouseenter',draw);
+        grid_cell.addEventListener('mouseup',isEndDrawing);
+
+    
+        grid.appendChild(grid_cell);
+    }
+    
+    return grid;
+
+}
+
+function updateGrid(){
+
+    new_grid = createNewGrid();
+    grid.remove()
+    grid_container.appendChild(new_grid)
+    grid = new_grid
+    
+}
+
+
+function getGridSize(){
+    grid_rows_columns = this.value
+    updateGrid()
+}
+
+
+
+createGridCells()
+
+
+
+
+
 
